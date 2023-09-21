a53e56d14b2ff5d46a6c039c8053d8bee7c3817b
radmirArs <142175910+radmirArs@users.noreply.github.com>
2023-09-21 09:59:38 +0500

    Update Program.cs

--- PZ_04_01/Program.cs
+++ PZ_04_01/Program.cs
@@ -4,8 +4,11 @@
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите диапазон от");
             int start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите диапазон до");
             int end = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите шаг");
             int step = Convert.ToInt32(Console.ReadLine());
             for(int i = start; i < end; i++)
             {
