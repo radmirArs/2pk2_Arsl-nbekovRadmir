708fccb37393f2ddf42e1106df5f541d5d0f1718
Радмир Арсоанбеков <radmirar00@mail.ru>
2023-09-21 10:11:24 +0500

    добавлена 4 практика задание 2

---
+++ PZ_04_02/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace PZ_04_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char start = 'a'; 
+            int n = Convert.ToInt32(Console.ReadLine()); 
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(start); 
+                start++; 
+            }
+        }
+    }
+}
