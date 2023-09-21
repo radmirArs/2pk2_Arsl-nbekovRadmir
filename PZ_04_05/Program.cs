c103978b1c54d7336ad13ed4089ef218c4f3263e
Радмир Арсоанбеков <radmirar00@mail.ru>
2023-09-21 10:28:30 +0500

    добавлена 4 практика 5 задание

---
+++ PZ_04_05/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PZ_04_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("значение i");
+            int i = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("значение j");
+            int j = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("разница");
+            int difference = Convert.ToInt32(Console.ReadLine()); 
+            while (Math.Abs(i - j) >= difference)
+            {
+                Console.WriteLine($"i = {i}, j = {j}"); 
+                i++; 
+                j--; 
+            }
+        }
+    }
+}
