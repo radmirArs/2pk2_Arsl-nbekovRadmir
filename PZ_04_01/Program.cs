fc4215e69e69e53fd2c72fddccdfaf22140acdb1
Радмир Арсоанбеков <radmirar00@mail.ru>
2023-09-21 09:58:11 +0500

    добавлена 4  практика номер 1

---
+++ PZ_04_01/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PZ_04_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = Convert.ToInt32(Console.ReadLine());
+            int end = Convert.ToInt32(Console.ReadLine());
+            int step = Convert.ToInt32(Console.ReadLine());
+            for(int i = start; i < end; i++)
+            {
+                
+                if(i%step == 0) 
+                {
+                    continue; 
+                }
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
