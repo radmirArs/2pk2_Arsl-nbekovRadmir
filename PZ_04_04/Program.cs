8fa44f3884e25d321f91510d8c789169421a0306
Радмир Арсоанбеков <radmirar00@mail.ru>
2023-09-21 10:21:53 +0500

    добавлена 4 практика задагие 4

---
+++ PZ_04_04/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace PZ_04_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите диапазон от");
+            int start = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите диапазон до");
+            int end = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Кратно какому числу");
+            int multiplicity = Convert.ToInt32(Console.ReadLine());
+            for (int i = start; i < end; i++)
+            {
+
+                if (i % multiplicity == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                
+            }
+        }
+    }
+}
