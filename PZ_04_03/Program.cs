bd8f2e20cbea79f3ea17281658811d735c32af2f
Радмир Арсоанбеков <radmirar00@mail.ru>
2023-09-21 10:18:30 +0500

    добавлена 4 практика 3 задание

---
+++ PZ_04_03/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace PZ_04_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("введите n");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("введите m");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write("#"); 
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
