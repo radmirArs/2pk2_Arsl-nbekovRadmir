51cdf3470f68fb22173668512ab91442e3527888
Радмир Арсоанбеков <radmirar00@mail.ru>
2023-09-22 13:36:28 +0500

    добавлена 5 практика

---
+++ PZ_05/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace PZ_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число N");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int number = 1;
+            do
+            {
+                int kv = number * number;
+                if (kv <= n)
+                {
+                    Console.Write(kv + " ");
+                }
+                number++;
+            } while (number * number <= n);
+        }
+    }
+}
