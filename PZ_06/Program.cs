3b73f9a73f9b498ea2f015732bf6eee7c2b924dd
radmirArs <142175910+radmirArs@users.noreply.github.com>
2023-09-27 15:46:03 +0500

    добавлена 6 практика

---
+++ PZ_06/Program.cs
@@ -0,0 +1,35 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace PZ_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите 10 чисел через пробел(n n1 n2 n3..n10");
+            string numbers = Console.ReadLine();
+            string [] num = numbers.Split(" ");
+            double[] numbersarray = new double[num.Length] ;
+            double num1 = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                numbersarray[i] = Convert.ToDouble(num[i]);
+                if (numbersarray[i] == 0 || i < 11) break;
+                num1 = num1 + numbersarray[i];
+            }
+            if (num1 > 0) { Console.WriteLine(num1);}
+            else { Console.WriteLine("ошибка"); }
+
+           /* Console.WriteLine("Введите значения 10-ти элементов массива: ");
+            double[] numbersarray = new double[10];
+            double num1 = 0;
+            for (int i = 0; i < numbersarray.Length; i++)
+            {
+                numbersarray[i] = Convert.ToDouble(Console.ReadLine());
+                if (numbersarray[i] == 0 && i <= 10) break;
+                num1 = num1 + numbersarray[i];
+            }
+            Console.WriteLine(num1);*/
+        }
+    }
+}
