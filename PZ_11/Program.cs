26aa6eab69a72838249f8aac7c8cd34515cffde8
radmirArs <142175910+radmirArs@users.noreply.github.com>
2023-11-25 13:27:44 +0500

    добавлена 11 практика

---
+++ PZ_11/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_11
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            double a = Convert.ToDouble(Console.ReadLine()); // первый катет
+            double b = Convert.ToDouble(Console.ReadLine()); ; // второй катет
+            double c = GetHypotenuse(a, b); // вгипотенуза
+            double area = GetArea(a, b); // площадь площадь
+            double inradius = GetInradius(a, b); // радиус вписанной окружности
+
+            Console.WriteLine("Гипотенуза: " + c);
+            Console.WriteLine("Площадь: " + area);
+            Console.WriteLine("Радиус вписанной окружности: " + inradius);
+        }
+
+        static double GetHypotenuse(double a, double b)
+        {
+            return Math.Sqrt(Math.Pow(a,2) * Math.Pow(b,2));
+        }
+
+        static double GetArea(double a, double b)
+        {
+            return (a * b) / 2;
+        }
+
+        static double GetInradius(double a, double b)
+        {
+            double c = GetHypotenuse(a, b);
+            return (a + b - c) / 2;
+        }
+    }
+}
---
+++ PZ_11/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PZ_11")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PZ_11")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("1a1ea77c-f9ac-402e-9570-ac89a452efc7")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
